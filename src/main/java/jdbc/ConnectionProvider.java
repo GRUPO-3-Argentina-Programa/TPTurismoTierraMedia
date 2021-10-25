fee718fafa1adedb8ed7bb4b303d618f0b8ab962
JoaquinYari <87427358+JoaquinYari@users.noreply.github.com>
2021-10-25 18:58:56 -0300

    fix Connection Provider

--- src/main/java/jdbc/ConnectionProvider.java
+++ src/main/java/jdbc/ConnectionProvider.java
@@ -6,13 +6,13 @@ import java.sql.SQLException;
 
 public class ConnectionProvider {
 	
-	private static String url = "jdbc:sqlite:C:\\joaco\\ARGENTINA PROGRAMA COSITAS\\TIERRA MEDIA TP GRUPAL\\DB"; 
+	private static String url = "jdbc:sqlite:C:\\joaco\\ARGENTINA PROGRAMA COSITAS\\TIERRA MEDIA TP GRUPAL\\DB\\turismoTierraMedia.db"; 
 
 	private static Connection connection;
 	
 	//jdbc:sqlite:C:\Users\JUAN PABLO\Desktop\ARGENTINA PROGRAMA\BD\turismoTierraMedia.db
 	//jdbc:sqlite:C:\Users\Diego\Argentina_Programa\SQL\turismoTierraMedia.db
-	//jdbc:sqlite:C:\joaco\ARGENTINA PROGRAMA COSITAS\TIERRA MEDIA TP GRUPAL\DB
+	//jdbc:sqlite:C:\joaco\ARGENTINA PROGRAMA COSITAS\TIERRA MEDIA TP GRUPAL\DB\turismoTierraMedia.db
 
 	public static Connection getConnection() throws SQLException {
 		 		if (connection == null) {
